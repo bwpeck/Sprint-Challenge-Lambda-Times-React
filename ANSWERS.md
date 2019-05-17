f95c23cc09274cd5f61130edabc143e2ca7b9b34
bwpeck <benjaminwade483@yahoo.com>
2019-05-17 14:39:36 -0400

    carousel done

---
+++ ANSWERS.md
@@ -0,0 +1,6 @@
+1. PropTypes are a dictionary where you define what props your component needs and what types it should have.
+2. a Lifecycle event declares special methods on the component class to run some code when a component mounts and unmounts.
+3. a Higher order function is a function that can take another function in as an argument, or returns a function as a result.
+4. a. Inline styling: we can create a variable that stores style properties and then pass it to the element, as well as pass it directly
+   b.CSS Modules: similar to CSS
+   c. Styled-Components: we can create a variable by selecting a particular HTML element where we store our style keys.
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel'
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
 
-export default class Content extends Component {
+class Content extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -28,18 +29,6 @@ export default class Content extends Component {
   }
 
   filterCards = () => {
-    /* Right now this function only returns the cards on state.
-      We're going to make this function more dynamic
-      by using it to filter out our cards for when a tab is selcted
-      
-      Notice that we're passing this function to our <Cards /> component below.
-      This function returns an array of cards, so we can just pass it down as such.
-
-      Your algorithim for the logic here is as follows: 
-        - if the selected tab is 'all' it should return all 
-          of the items from cardData. 
-        - else, it should only return those cards whose 'tab' matched this.state.selected.
-    */
    const cards = this.state.cards.filter(card => {
      if(this.state.selected === "all") {
       return this.state.cards;
@@ -53,9 +42,11 @@ export default class Content extends Component {
   render() {
     return (
       <div className="content-container">
+      <Carousel/>
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.selectTabHandler} />
         <Cards cards={this.filterCards()} />
       </div>
     );
   }
 }
+ export default Content
